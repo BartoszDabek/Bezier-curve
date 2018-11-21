54cec33216a5c21c083a64f1851dfdfd159d1972
Bartosz Dąbek <elderaramka@gmail.com>
2018-11-21 18:03:32 +0100

    import basic layout from paint project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
 public class Controller {
+
+    private static final double mainWidth = 1200;
+    private static final double mainHeight = 600;
+
+    @FXML
+    private Canvas mainCanvas;
+    @FXML
+    private StackPane canvasHolder;
+    @FXML
+    private Label coordinates;
+
+    @FXML
+    public void initialize() {
+        canvasHolder.setOnMouseMoved(event -> coordinates.setText("X: " + event.getX() + "  Y: " + event.getY()));
+        canvasHolder.setMaxHeight(mainHeight);
+        canvasHolder.setMaxWidth(mainWidth);
+
+        GraphicsContext gc = mainCanvas.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, this.mainCanvas.getWidth(), this.mainCanvas.getHeight());
+
+        mainCanvas.setHeight(mainHeight);
+        mainCanvas.setWidth(mainWidth);
+    }
 }
